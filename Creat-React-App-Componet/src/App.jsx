50db86a967086133b2a06f0821b0d570d2e15044
rishisingh <rishicoding9838@gmail.com>
2024-03-06 00:48:45 +0530

    first commit

---
+++ Creat-React-App-Componet/src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import RishiButton from "./RishiButton";
+function App(){
+  return <div>
+       <h1>
+           My name is Rishi singh.
+      </h1>
+      {/* <button>Subscribe</button> */}
+      <RishiButton>subscribe</RishiButton>
+  </div>
+
+   
+}
+
+export default App;
---
+++ Creat-React-App-Componet/src/RishiButton.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line react-refresh/only-export-components, no-unused-vars
+function RishiButton(){
+  return <button>Like this Program</button>
+}
+
+export default RishiButton;
